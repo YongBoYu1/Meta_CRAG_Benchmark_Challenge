3f3f26beb73c826d9878c5fd75defdd7fba36566
yoyongbo <yoyu@upei.ca>
2024-04-25 11:18:12 -0400

    Using 2 different loader to try urls

--- baseline_model.py
+++ baseline_model.py
@@ -3,15 +3,17 @@ import os
 from dotenv import load_dotenv
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_openai import ChatOpenAI
-from langchain_community.document_loaders import  SeleniumURLLoader
+from langchain_community.document_loaders import  SeleniumURLLoader, WebBaseLoader
 from langchain_community.vectorstores import Chroma
 from langchain_openai import OpenAIEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains import create_retrieval_chain
-
+import time
+import requests
 # Load the .env file which contains the API keys
 load_dotenv()
+
 
 # Set the API keys as environment variables
 os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
@@ -27,7 +29,7 @@ with open('data.pkl', 'rb') as f:
 print(len(data))
 
 # Limit the data to 300, because the API key are Expensive!!
-data = data[:300]
+# data = data[:300]
 
 
 # Define the llm. Here we just use the ChatOpenAI class from langchain_openai.
@@ -44,6 +46,7 @@ prompt = ChatPromptTemplate.from_template(
 document_chain = create_stuff_documents_chain(llm, prompt)
 
 result_dict = {}
+result_dict['docs'] = []
 result_dict['query'] = []
 result_dict['answer'] = []
 result_dict['sys_ans'] = []
@@ -58,35 +61,70 @@ def build_db_from_url(urls):
         _type_: _description_
     """
     # Load the documents from the urls
-    loader = SeleniumURLLoader(urls)
-    doc = loader.load()
+    # docs = []
+    # for url in urls:
+    #     print(f"Loading URL: {url}")
+    #     docs = []
+    #     try:
+    #         loader = SeleniumURLLoader(
+    #         urls = url,
+    #         arguments=[
+    #             "log-level=3",
+    #         ])
+    #         doc = loader.load()
+    #         docs.append(doc)
+    #         # process the loaded document here
+    #         print(f"Loaded URL: {url}")
+    #     except Exception as e:
+    #         print(f"Error loading URL: {url} - {str(e)}")   
 
-    text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
-        chunk_size=500, chunk_overlap=0
-    )
 
-    doc_splits = text_splitter.split_documents(doc)
+    docs = []
+    for url in urls:
+        try:
+            docs.append(WebBaseLoader(url).load())
+            time.sleep(1) # Adjust the sleep time as needed
+        except (requests.exceptions.ConnectionError):
+            print(f"Error loading {url}: Connection aborted. Retrying...")
 
-    # Add to vectorDB
-    vectorstore = Chroma.from_documents(
-        documents=doc_splits,
-        collection_name="rag-chroma",
-        embedding=OpenAIEmbeddings(),
-    )       
-    return vectorstore
+            # Retry the request with a delay (e.g., 2-5 seconds)
+            time.sleep(2)
+            docs.append(SeleniumURLLoader(url).load())
+
+    docs_list = [item for sublist in docs for item in sublist]
+    print(f"Total number of documents: {len(docs_list)}")
+
+    if len(docs_list) >0:
+        
+        text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+            chunk_size=500, chunk_overlap=0
+        )
+        doc_splits = text_splitter.split_documents(docs_list)
+
+        # Add to vectorDB
+        vectorstore = Chroma.from_documents(
+            documents=doc_splits,
+            collection_name="rag-chroma",
+            embedding=OpenAIEmbeddings(),)
+        return vectorstore
+    else: 
+        print(f"Total number of documents: {len(docs_list)}")
+        print(docs_list) 
+        return None
 
 
 print("Starting the loop...")
 # Loop through the data and get the answer from the model
 # We build the vector db from the urls privuded by each data
 # and then use the model to get the answer.
-for i, doc_point in enumerate(data):
-
+for i, doc_point in enumerate(data[:30]):
+    print(f"Processing document {i+1}...")
     # Extract the query, answer and search_results
     query = doc_point['query']
     answer_Y = doc_point['answer']
     docs_url = []
     docs_snippet = []
+    
 
     # Extract the urls and snippets from the search_results
     search_result_list = doc_point['search_results']
@@ -95,15 +133,23 @@ for i, doc_point in enumerate(data):
         docs_snippet.append(item['page_snippet'])
 
     retriever = build_db_from_url(docs_url)
-    retriever = retriever.as_retriever()
+    if retriever:
+        retriever = retriever.as_retriever()
 
-    retrieval_chain = create_retrieval_chain(retriever, document_chain)
-    
-    response = retrieval_chain.invoke({"input": query})
-    sys_ans = response['answer']
-    result_dict['query'].append(query)
-    result_dict['answer'].append(answer_Y)
-    result_dict['sys_ans'].append(sys_ans)
+        # The document that used to retrieve the answer
+        docs = retriever.invoke(query)
+
+        retrieval_chain = create_retrieval_chain(retriever, document_chain)
+        
+        response = retrieval_chain.invoke({"input": query})
+        sys_ans = response['answer']
+
+        result_dict['docs'].append(docs)
+        result_dict['query'].append(query)
+        result_dict['answer'].append(answer_Y)
+        result_dict['sys_ans'].append(sys_ans)
+    else:
+        print(f"No documents to retrieve from. Skipping query...{query}")
 
 
                                 
@@ -111,3 +157,7 @@ for i, doc_point in enumerate(data):
 print("Saving the result_dict...")
 with open('result.pkl', 'wb') as f:
     pkl.dump(result_dict, f)
+
+print("Done!")
+
+# todo: Get some example url to test the build_db_from_url function
